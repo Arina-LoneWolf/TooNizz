120c279eb0ee3293dcf1e95db32e6d80a78bbfe6
Arina <petrinhtrinh182@gmail.com>
2021-11-18 11:33:08 +0700

    style header

--- client/src/shared/Header/index.jsx
+++ client/src/shared/Header/index.jsx
@@ -3,35 +3,33 @@ import React from 'react';
 import { IoSearchOutline } from 'react-icons/io5';
 import { IoMdNotificationsOutline } from 'react-icons/io';
 import { BsList } from 'react-icons/bs';
+import { RiNotification3Line } from 'react-icons/ri';
+import { IoNotificationsOutline } from 'react-icons/io5';
+
+const avatar = 'https://i.pinimg.com/564x/0b/97/f5/0b97f53a14d3b0698c1388b013ecabfa.jpg';
 
 function Header() {
   return (
     <div className="header">
-      <div className="side-btn">
-        <BsList className="side-icon" />
-      </div>
+      <BsList className="side-btn" />
 
       <div className="logo">LOGO</div>
 
-      <div className="create-btn">Create</div>
+      <div className="create-btn">Create +</div>
 
       <div className="search-bar">
         <input placeholder="Search" />
         <IoSearchOutline className="search-icon" />
       </div>
 
+      <div className="join-btn">Join</div>
+
       <div className="btn-group">
-        <div className="enter-code-btn">Enter code</div>
+        <IoSearchOutline className="search-btn" />
 
-        <div className="search-btn">
-          <IoSearchOutline className="search-icon" />
-        </div>
+        <IoNotificationsOutline className="noti-icon" />
 
-        <div className="notifications">
-          <IoMdNotificationsOutline className="notification-icon" />
-        </div>
-
-        <div className="avatar"></div>
+        <div className="avatar" style={{ backgroundImage: `url(${avatar})` }}></div>
       </div>
     </div>
   );
